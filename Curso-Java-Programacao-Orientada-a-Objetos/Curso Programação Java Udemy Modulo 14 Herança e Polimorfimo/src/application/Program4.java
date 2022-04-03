029e325fec028bc7b5b29772d2fbddd50ab9e06f
emmnesty <elyjhonatan@hotmail.com>
2022-04-03 00:20:26 -0300

    Herança e Polimorfismo

---
+++ Curso-Java-Programacao-Orientada-a-Objetos/Curso Programação Java Udemy Modulo 14 Herança e Polimorfimo/src/application/Program4.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		BusinessAccount account = new BusinessAccount();
+		
+		
+
+	}
+
+}
